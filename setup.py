2bbc8b883c8468542419f58305a14a12dbd4a147
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2024-08-16 23:29:03 +0200

    Update CI matrix

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
     author=__author__,
     author_email='jakub@roztocil.co',
     license=__licence__,
-    url='https://github.com/jakubroztocil/httpcat',
-    download_url='https://github.com/jakubroztocil/httpcat',
+    url='https://github.com/httpie/httpcat',
+    download_url='https://github.com/httpie/httpcat',
     py_modules=[
         'httpcat',
     ],
